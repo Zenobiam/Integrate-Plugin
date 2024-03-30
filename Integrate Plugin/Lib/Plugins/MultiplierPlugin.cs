0c19c8e444d9066e27f03e5be934bcb6a1c19849
Nikita Filyaeff <nikita.filyaeff@yandex.ru>
2024-03-30 15:57:28 +0300

    Inititate Plugin Base structure

---
+++ Integrate Plugin/Lib/Plugins/MultiplierPlugin.cs
@@ -0,0 +1,21 @@
+﻿using ds.test.impl.Base;
+using System.Drawing;
+
+namespace ds.test.impl.Plugins
+{
+    internal class MultiplierPlugin : PluginBase
+    {
+        public override string PluginName => "Multiplier";
+
+        public override string Version => "0.1v";
+
+        public override Image Image => null;
+
+        public override string Description => "Multiplies two numbers";
+
+        public override int Run(int input1, int input2)
+        {
+            return input1 * input2;
+        }
+    }
+}
