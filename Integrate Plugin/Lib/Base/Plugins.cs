c41b9eb22a9f7f3755af7a1ae9e463760127f3c0
Nikita Filyaeff <nikita.filyaeff@yandex.ru>
2024-03-30 15:59:19 +0300

    Revert "Revert "Inititate Plugin Base structure""
    This reverts commit 2c5a7ff41436a7288e15778e26283467421315b7.

---
+++ Integrate Plugin/Lib/Base/Plugins.cs
@@ -0,0 +1,36 @@
+﻿using ds.test.impl.Interfaces;
+
+namespace ds.test.impl.Base
+{
+    /// <summary>
+    /// Статический класс, предоставляющий доступ к плагинам.
+    /// </summary>
+    public static class Plugins
+    {
+        /// <summary>
+        /// Словарь, в котором содержаться плагины, где ключ - имя плагина, а значение - экземпляр плагина IPlugin
+        /// </summary>
+        private static Dictionary<string, IPlugin> _plugins = new Dictionary<string, IPlugin>();
+
+        /// <summary>
+        /// Возвращает количество зарегистрированных плагинов
+        /// </summary>
+        public static int PluginsCount => _plugins.Count;
+
+        /// <summary>
+        /// Возвращает имя зарегистрированного плагина
+        /// </summary>
+        public static string[] GetPluginNames => _plugins.Keys.ToArray();
+
+        /// <summary>
+        /// Получение экземпляра плагина по его имени
+        /// </summary>
+        public static IPlugin GetPlugin(string pluginName)
+        {
+            if (_plugins.ContainsKey(pluginName))
+                return _plugins[pluginName];
+            else
+                throw new ArgumentException("Plugin not found");
+        }
+    }
+}
---
+++ Integrate Plugin/Lib/Plugins/SumPlugin.cs
@@ -0,0 +1,21 @@
+﻿using ds.test.impl.Base;
+using System.Drawing;
+
+namespace ds.test.impl.Plugins
+{
+    internal class SumPlugin : PluginBase
+    {
+        public override string PluginName => "Sum Plugin";
+
+        public override string Version => "0.1v";
+
+        public override Image Image => null;
+
+        public override string Description => "Sum two numbers";
+
+        public override int Run(int input1, int input2)
+        {
+            return input1 + input2;
+        }
+    }
+}
